25d4834e978593a5756a9299aeb79423b7cbd270
SvMaag <sven.maag@gmail.com>
2025-02-16 17:12:11 +0100

    JS Object

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let myObject = {
+    'name':'flo',
+    'age': 45,
+    'logJob': function(number){
+        console.log('Dev-Mentor' + number  )          
+    },
+    'good_guy':true,
+};
+
+
+myObject.logJob(555)
+
+
+
+//console.table(myObject);
